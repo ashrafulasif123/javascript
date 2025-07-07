038e5f9677cdb55edb429db6869c123a5efc5fcf
ashrafulasif123 <cseasifiiuc9@gmail.com>
2025-07-07 11:33:47 +0600

    from laptop

--- javascript-basic/practise.js
+++ javascript-basic/practise.js
@@ -1,29 +1,14 @@
-// const element = [
-//     {
-//         rice: '2kg',
-//         price: 140,
-//         quantity: 2
-//     },
-//     {
-//         flour: '1kg',
-//         price: 60,
-//         quantity: 3
-//     },
-//     {
-//         wheelPowder: '2kg',
-//         price: 50,
-//         quantity: 4
-//     }
-// ]
+const removeDuplicate = (sentence) => {
+    const sentenceSplit = sentence.split(' ')
+    const uniqueValue = []
+    for (const sent of sentenceSplit) {
+        if (uniqueValue.includes(sent) === false) {
+            uniqueValue.push(sent)
+        }
+    }
+    console.log(uniqueValue.join(' '))
+}
 
-// function market(element){
-//     let totalPrice = 0;
-//     for(const ele of element){
-//         const inTotal = ele.price * ele.quantity
-//         totalPrice = totalPrice + inTotal
-//     }
-//     return totalPrice
-// }
 
-// console.log(market(element))
-
+const sentence = 'i was graduated from was international islamic university islamic chittagong in 2015 2015'
+const resultRemoveDuplicate = removeDuplicate(sentence)
