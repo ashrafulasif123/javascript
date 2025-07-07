27ccae2dea9807b7b62cfa35c3c6fa37fb29531a
ashrafulasif123 <cseasifiiuc9@gmail.com>
2025-07-07 16:49:08 +0600

    rest in

--- javascript-basic/practise.js
+++ javascript-basic/practise.js
@@ -1,3 +1,17 @@
-const versity = `i was graduated   from international islamic university chittagong in 2015`
-// const cleaned = versity.trim().split(/\s+/).join(' ')
-console.log(versity)
+// const car = {make : "Toyota", model: "Corolla", year: 2020} এই অবজেক্টের একটি কপি তৈরী কর এবং Year Update করে  2032 করে নতুন অবজেক্ট তৈরী কর
+
+const car = { make: "Toyota", model: "Corolla", year: 2020 }
+const { ...newCar } = car;
+newCar.year = 2025
+console.log(car)
+console.log(newCar)
+
+// const car = { make: "Toyota", model: "Corolla", year: 2020 }
+// const { ...newCar } = car;
+// newCar.year = 2025
+// console.log(car)
+// console.log(newCar)
+
+
+
+
